15db16106a5e16b13acb55dba32ffd2a3d622a20
Jeremiah Brown <jeremiahbrown1288@gmail.com>
2026-09-17 00:31:21 -0400

    Define variables for Azure infrastructure
    Added variables for user name, location, tags, and passwords.

--- Part Two: Azure Infrastructu/variables.tf
+++ Part Two: Azure Infrastructu/variables.tf
@@ -1,1 +1,29 @@
+variable "yourname" {
+  type = string
+}
+ 
+variable "location" {
 
+ description = "Azure region for target resources. Choose a region close to your AWS region."
+  type        = string
+  default     = "East US"
+}
+ 
+variable "tags" {
+  type = map(string)
+  default = {
+    project    = "azure-migrate-lab"
+    managed_by = "terraform"
+  }
+}
+variable "appliance_admin_password" {
+  description = "Administrator password for the appliance."
+  type        = string
+  sensitive   = true
+}
+
+variable "replication_admin_password" {
+  description ="Admin password for the replicationappliance VM."
+  type        = string
+  sensitive   = true
+}
